c8ab9db95e9f61c13e9dee6c16a29f697551441d
TOEYTANAPA <akanip2@gmail.com>
2017-12-10 17:45:55 +0700

    upload model picture

--- cs401_project/settings.py
+++ cs401_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'widget_tweaks',
     'mainapp',
     'loginapp',
@@ -44,6 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.postgres' 
 
 ]
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
